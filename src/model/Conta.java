10a6f6ff2b800f7aced8a17799efc14d729ce6e7
Mariane Santos <marydoctor221@gmail.com>
2024-08-20 20:39:22 -0300

    Modelagem

---
+++ src/model/Conta.java
@@ -0,0 +1,27 @@
+
+
+public class Conta {
+    //atributos
+    public String conta;
+    public String agencia;
+    public double saldo;
+    public String nomeCliente;
+
+    //metodos
+    public int sacar(double valor){
+
+    }
+
+    public void depositar(double deposito){
+
+    }
+
+    public void imprimir(){
+        System.out.println("Conta:         " + conta);
+        System.out.println("Agencia:       " + agencia);
+        System.out.println("Saldo:         " + saldo);
+        System.out.println("Nome Cliente:  " + nomeCliente);
+    }
+}
+
+
