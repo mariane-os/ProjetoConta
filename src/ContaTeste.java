5b6113d5b20f2715265916544ac7045f192ea531
Mariane Santos <marydoctor221@gmail.com>
2024-08-20 22:16:43 -0300

    Codigo pronto

--- src/ContaTeste.java
+++ src/ContaTeste.java
@@ -1,22 +1,78 @@
+
+import java.util.Scanner;
+
 public class ContaTeste {
-    public Conta cc = new Conta(); 
+    public Conta cc = new Conta();
+    public Scanner leitor = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        ContaTeste teste = new ContaTeste();
+
+        int opc = 9;
+
+        do{
+        System.out.println("Menu");
+        System.out.println("1. Cadastrar");
+        System.out.println("2. Depositar");
+        System.out.println("3. Consultar");
+        System.out.println("4. Sacar");
+        System.out.println("9. Sair");
+        System.out.println("Digite sua opção");
+
+        opc = Integer.parseInt(teste.leitor.nextLine());
+        switch (opc) {
+            case 1:
+                teste.execCadastrar();
+                break;
+            case 2   :
+                teste.execDepositar();
+                break;
+            case 3:
+                teste.execConsultar();
+                break;
+            case 4:
+                teste.execSacar();     
+                break;
+            case 9:
+                System.out.println("Saindo...");
+                break; 
+            default:
+            System.out.println("Opção inválida!");
+        }
+
+        }while(opc!=9);
     } 
 
     public void execCadastrar(){
-
+        System.out.println("Digite a agência: ");
+        cc.agencia = leitor.nextLine();
+        System.out.println("Digite a conta: ");
+        cc.conta = leitor.nextLine();
+        System.out.println("Digite a nome: ");
+        cc.nomeCliente = leitor.nextLine();
+        System.out.println("Conta criada com sucesso!");
     }
 
     public void execConsultar(){
-
+        cc.imprimir();
     }
 
     public void execSacar(){
-
+        double valor;
+        System.out.println("Digite o valor do saque:");
+        valor = Double.parseDouble(leitor.nextLine());
+        if(cc.sacar(valor)){
+        System.out.println("Saque realizado com sucesso!");
+        }else{
+        System.out.println("Sem saldo para realizar saque");
+        }
     }
 
     public void execDepositar(){
-        
+        double valor;
+        System.out.println("Digite o valor do deposito: ");
+        valor = Double.parseDouble(leitor.nextLine());
+        cc.depositar(valor);
+        System.out.println("Depositado com sucesso!");
     }
 }
--- src/model/Conta.java
+++ src/model/Conta.java
@@ -6,18 +6,27 @@ public class Conta {
     public String nomeCliente;
 
     //metodos
-    public int sacar(double valor){
+    public boolean sacar(double valor){
+        if(valor <= saldo){
+            saldo -= valor;
+            return true;
+        }
+        return false;
     }
 
     public void depositar(double deposito){
-
+        saldo += deposito;
     }
 
     public void imprimir(){
+        System.out.println("---------------------------");
         System.out.println("Conta:         " + conta);
         System.out.println("Agencia:       " + agencia);
         System.out.println("Saldo:         " + saldo);
         System.out.println("Nome Cliente:  " + nomeCliente);
+        System.out.println("---------------------------");
+
+
     }
 }
 
